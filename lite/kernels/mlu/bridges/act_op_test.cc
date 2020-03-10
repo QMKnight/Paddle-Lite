59d4f64ec09a3c9286ba87e69b5c08687eac76b2
jackzhang235 <zhangmingwei@cambricon.com>
2020-03-10 09:12:46 +0000

    register for sigmoid and tanh

--- lite/kernels/mlu/bridges/act_op_test.cc
+++ lite/kernels/mlu/bridges/act_op_test.cc
@@ -134,7 +134,7 @@ void test_act(std::vector<int64_t> x_shape, std::string op_type) {
 
 TEST(MLUBridges, activation) {
   std::vector<std::vector<int64_t>> shapes{{1}, {2, 3}, {1, 2, 3, 4}};
-  std::vector<std::string> types{"relu"};
+  std::vector<std::string> types{"sigmoid", "relu", "tanh"};
   for (auto x_shape : shapes) {
     for (auto op_type : types) {
       test_act(x_shape, op_type);
@@ -147,4 +147,6 @@ TEST(MLUBridges, activation) {
 }  // namespace lite
 }  // namespace paddle
 
+USE_SUBGRAPH_BRIDGE(sigmoid, kMLU)
 USE_SUBGRAPH_BRIDGE(relu, kMLU)
+USE_SUBGRAPH_BRIDGE(tanh, kMLU)
